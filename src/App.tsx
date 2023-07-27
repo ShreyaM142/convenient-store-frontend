7036b81e6301c96462721c2c936940834234b058
Lokenath Karmakar <ilokenathkarmakar@gmail.com>
2023-07-27 10:50:16 +0000

    setup mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -30,6 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <Typography fontWeight="bold">Hello world</Typography>
+      <Button>Hello world</Button>
     </>
   );
 }
